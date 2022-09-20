d41c9ec5953bad339e6148a3340160e992be37f8
Max von Webel <max@343max.de>
2022-09-20 16:16:05 +0200

    dynamic port number

--- bin-src/lib/startMetroCommand.ts
+++ bin-src/lib/startMetroCommand.ts
@@ -2,8 +2,15 @@ import Bonjour from 'bonjour-service';
 import { command, number, option, optional } from 'cmd-ts';
 import { readPackageJson } from './package-json';
 import { sleep } from './sleep';
-// import { hostname } from 'os';
 import { execSync } from 'child_process';
+import { createHash } from 'crypto';
+
+const generatePort = (): number => {
+  // generate a stable port number based on the current path
+  const path = process.cwd();
+  const hash = createHash('sha256').update(path).digest('hex');
+  return (parseInt(hash.substring(0, 8), 16) % 600) + 8100;
+};
 
 export const startMetroCommand = command({
   name: 'start-metro',
@@ -16,15 +23,13 @@ export const startMetroCommand = command({
     }),
   },
   handler: async ({ customPort }) => {
-    const port = customPort ?? 8080;
+    const port = customPort ?? generatePort();
     const bonjour = new Bonjour();
     const packageJson = await readPackageJson();
     const appName = `${packageJson.name}`;
     const computerName = execSync('scutil --get ComputerName')
       .toString()
       .trim();
-
-    console.log(computerName);
 
     process.on('SIGINT', () => {
       console.log('exit');
@@ -46,7 +51,7 @@ export const startMetroCommand = command({
         },
       });
       console.log('listening...');
-      await sleep(1000);
+      await sleep(10000);
       console.log('off');
       await unpublishAll();
       await sleep(1000);
